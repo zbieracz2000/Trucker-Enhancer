981c3c799b6bd7a9acd324d7230c2084cfc4c580
zbieracz2000 <igor.bondaryk@aligstor.pl>
2024-11-19 23:35:47 +0100

    Dodaj pliki projektów.

---
+++ Trucker Enchancer/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Trucker_Enchancer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.run = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.money = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.money)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // run
+            // 
+            resources.ApplyResources(this.run, "run");
+            this.run.Name = "run";
+            this.run.UseVisualStyleBackColor = true;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            resources.ApplyResources(this.checkBox2, "checkBox2");
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // money
+            // 
+            resources.ApplyResources(this.money, "money");
+            this.money.Maximum = new decimal(new int[] {
+            -1,
+            0,
+            0,
+            0});
+            this.money.Name = "money";
+            this.money.Value = new decimal(new int[] {
+            20000,
+            0,
+            0,
+            0});
+            this.money.ValueChanged += new System.EventHandler(this.money_ValueChanged);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.money);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.run);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.money)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown money;
+    }
+}
+
---
+++ Trucker Enchancer/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+
+
+
+namespace Trucker_Enchancer
+{
+
+    public partial class Form1 : Form
+    {
+        string path;
+        bool no_intro;
+        bool custom_music;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    path = fbd.SelectedPath;
+                    if (System.IO.File.Exists(path + "\\trucker.exe") == true)
+                    {
+                        label1.Text = path;
+                    }
+                    else
+                    {
+                        System.Windows.Forms.MessageBox.Show("Nie znaleziono gry w tym folderze!", "Trucker Enchancer");
+                        path = null;
+                    }
+                }
+            }
+        }
+
+        private void run_Click(object sender, EventArgs e)
+        {
+            if (path == null)
+            {
+                System.Windows.Forms.MessageBox.Show("Ustaw ścieżkę do folderu z grą!", "Trucker Enchancer");
+            }
+            else 
+            {
+                Int32 kasa = (int)money.Value;
+                byte[] bytes = BitConverter.GetBytes(kasa);
+
+                if (no_intro == true)
+                {
+                    using (var stream = new FileStream(path + "\\trucker.exe", FileMode.Open, FileAccess.ReadWrite))
+                    {
+                        stream.Position = 1303556;
+                        stream.WriteByte(0x0C);
+                        stream.Position = 1303568;
+                        stream.WriteByte(0x00);
+                        stream.Position = 1303570;
+                        stream.WriteByte(0x00);
+                        stream.Position = 1303572;
+                        stream.WriteByte(0x00);
+                        stream.Position = 1303574;
+                        stream.WriteByte(0x00);
+                        stream.Position = 1303576;
+                        stream.WriteByte(0x00);
+                        stream.Position = 1303578;
+                        stream.WriteByte(0x00);
+                        stream.Position = 1303580;
+                        stream.WriteByte(0x00);
+                        stream.Position = 1303582;
+                        stream.WriteByte(0x09);
+                    }
+                }
+                if (no_intro == false)
+                {
+                    using (var stream = new FileStream(path + "\\trucker.exe", FileMode.Open, FileAccess.ReadWrite))
+                    {
+                        stream.Position = 1303556;
+                        stream.WriteByte(0x6C);
+                        stream.Position = 1303568;
+                        stream.WriteByte(0x6C);
+                        stream.Position = 1303570;
+                        stream.WriteByte(0x6F);
+                        stream.Position = 1303572;
+                        stream.WriteByte(0x67);
+                        stream.Position = 1303574;
+                        stream.WriteByte(0x6F);
+                        stream.Position = 1303576;
+                        stream.WriteByte(0x2E);
+                        stream.Position = 1303578;
+                        stream.WriteByte(0x61);
+                        stream.Position = 1303580;
+                        stream.WriteByte(0x76);
+                        stream.Position = 1303582;
+                        stream.WriteByte(0x69);
+                    }
+                }
+                if (custom_music == true)
+                {
+                    using (var stream = new FileStream(path + "\\trucker.exe", FileMode.Open, FileAccess.ReadWrite))
+                    {
+                        stream.Position = 1334578;
+                        stream.WriteByte(0x63);
+                        stream.Position = 1334579;
+                        stream.WriteByte(0x75);
+                        stream.Position = 1334580;
+                        stream.WriteByte(0x73);
+                        stream.Position = 1334581;
+                        stream.WriteByte(0x74);
+                        stream.Position = 1334582;
+                        stream.WriteByte(0x6F);
+                        stream.Position = 1334583;
+                        stream.WriteByte(0x6D);
+                        stream.Position = 1334584;
+                        stream.WriteByte(0x00);
+                        stream.Position = 1334585;
+                        stream.WriteByte(0x00);
+                        stream.Position = 1334586;
+                        stream.WriteByte(0x00);
+                        stream.Position = 1334587;
+                        stream.WriteByte(0x00);
+                    }
+                }
+                if (custom_music == false)
+                {
+                    using (var stream = new FileStream(path + "\\trucker.exe", FileMode.Open, FileAccess.ReadWrite))
+                    {
+                        stream.Position = 1334578;
+                        stream.WriteByte(0x68);
+                        stream.Position = 1334579;
+                        stream.WriteByte(0x79);
+                        stream.Position = 1334580;
+                        stream.WriteByte(0x70);
+                        stream.Position = 1334581;
+                        stream.WriteByte(0x65);
+                        stream.Position = 1334582;
+                        stream.WriteByte(0x72);
+                        stream.Position = 1334583;
+                        stream.WriteByte(0x61);
+                        stream.Position = 1334584;
+                        stream.WriteByte(0x76);
+                        stream.Position = 1334585;
+                        stream.WriteByte(0x65);
+                        stream.Position = 1334586;
+                        stream.WriteByte(0x72);
+                        stream.Position = 1334587;
+                        stream.WriteByte(0x73);
+                    }
+                }
+                using (var stream = new FileStream(path + "\\trucker.exe", FileMode.Open, FileAccess.ReadWrite))
+                {
+                    stream.Position = 508444;
+                    stream.WriteByte(bytes[0]);
+                    stream.Position = 508445;
+                    stream.WriteByte(bytes[1]);
+                    stream.Position = 508446;
+                    stream.WriteByte(bytes[2]);
+                    stream.Position = 508447;
+                    stream.WriteByte(bytes[3]);
+                }
+                System.Windows.Forms.MessageBox.Show("Spatchowano!", "Trucker Enchancer");
+            }
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            no_intro = checkBox1.Checked;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+           custom_music = checkBox2.Checked;
+        }
+
+        private void money_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
